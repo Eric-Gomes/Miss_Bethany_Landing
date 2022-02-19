7a58afd7b5017a163284054a37191ea7598814e3
Eric Gomes <eric.n.gomes@hotmail.com>
2022-02-19 13:16:07 +0000

    Initialized Components for App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { IntroComponent } from './Components/intro/intro.component';
+import { NavComponent } from './Components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    IntroComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
